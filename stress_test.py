4b0249406dd650b4ea54bac27ea50da3021034d0
eyeve <astralolia@yandex.ru>
2024-03-19 01:12:29 +0000

    final version

--- stress_test.py
+++ stress_test.py
@@ -2,48 +2,54 @@ from os import system
 from sys import argv
 from re import sub
 
-N = 1000000
-OPERATION = '*'
+N = 15000
+OPERATIONS = ['*', '/', '-', '+']
 
 if (len(argv) > 1):
-    OPERATION = argv[1]
-    if (OPERATION != '*' and OPERATION != '/' and OPERATION != '+' and OPERATION != '-'):
-        print(f"Invalid operation: {OPERATION}")
-        exit(1)
+    OPERATIONS = []
+    for opeartion in argv[1:]:
+        OPERATIONS.append(opeartion)
+        if (opeartion != '*' and opeartion != '/' and opeartion != '+' and opeartion != '-'):
+            print(f"Invalid operation: {opeartion}")
+            exit(1)
 
 system("clang main.c -o main.exe")
 system("clang tester.c -o tester.exe")
 open("out.txt", "w")
 
-for testNumber in range(N):
-    system(f"./tester.exe '{OPERATION}' > out.txt")
+for operation in OPERATIONS:
+    correct = 0
+    for testNumber in range(N):
+        system(f"./tester.exe '{operation}' > out.txt")
 
-    with open("out.txt", "r") as f:
-        test = f.readline().strip()
-        expected = f.readline().strip()
-        decimal = f.readline().strip()
+        with open("out.txt", "r") as f:
+            test = f.readline().strip()
+            expected = f.readline().strip()
+            decimal = f.readline().strip()
 
-    system(f"./main.exe {test} > out.txt")
-    test = test.replace("'", "", 2)
-    with open("out.txt", "r") as f:
-        actual = f.readline().strip()
+        system(f"./main.exe {test} > out.txt")
+        test = test.replace("'", "", 2)
+        with open("out.txt", "r") as f:
+            actual = f.readline().strip()
 
-    if sub("0+p", "p", actual) != sub("0p", "0.p", expected):
-        print()
-        print(f"==== test failed => | {testNumber}")
-        print(f" \==== expected ==> | {expected}")
-        print(f"  \=== actual ====> | {actual}")
-        print(f"   \== test case => | {test}")
-        input(f"    \= decimal: {decimal}\n")
-    else:
-        if (testNumber % 100 == 0):
-            print(f"\n======== ONE HUNDRED MORE TESTS ========\n")
-        elif (testNumber % 10 == 0):
-            i = testNumber % 3
-            print("="*i + f"===> test passed - {testNumber}")
-    
+        if sub("0+p", "p", actual) != sub("0p", "0.p", expected):
+            print()
+            print(f"==== test failed => | {testNumber}")
+            print(f" \==== expected ==> | {expected}")
+            print(f"  \=== actual ====> | {actual}")
+            print(f"   \== test case => | {test}")
+            if input(f"    \= decimal: {decimal}\n") ==  "q":
+                break
+        else:
+            correct += 1
+            if (testNumber % 100 == 0):
+                print(f"\n======== ONE HUNDRED MORE TESTS ========\n")
+            elif (testNumber % 10 == 0):
+                i = testNumber % 3
+                print("="*i + f"===> test passed - {testNumber}")
 
+    print("\n========== ALL TESTS COMPLETED ==========")
+    print( f"             operation was {operation}")
+    print( f"                 {correct / N * 100}%")
+    input( f"           press any button...\n")
 
-
-
-    
